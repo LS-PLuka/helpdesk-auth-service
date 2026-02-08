07bd1ac8f75d506ba1daeb9369f398b4d1072b7b
Pedro Luka <pedrolukagt@gmail.com>
2026-02-08 19:05:19 -0300

    first commit

---
+++ src/main/java/com/helpdesk_auth_service/HelpdeskAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.helpdesk_auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpdeskAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpdeskAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helpdesk_auth_service/HelpdeskAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helpdesk_auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
